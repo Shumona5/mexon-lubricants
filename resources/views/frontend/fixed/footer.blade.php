506109ffa79b985ff4b94b725fb15e28b2e030c1
Shumona5 <shumonashikha@gmail.com>
2024-02-24 17:03:24 +0600

    Templating

---
+++ resources/views/frontend/fixed/footer.blade.php
@@ -0,0 +1,54 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 wow animate__fadeInUp" data-wow-duration="2s" data-wow-delay="0.1s" data-wow-offset="100">
+          <a href="index.html">
+            <img src="./assets/images/mexon_logo.png" alt="" width="100px">
+          </a>
+        </div>
+
+        <div class="col-md-3 wow animate__fadeInUp" data-wow-duration="2s" data-wow-delay="0.3s" data-wow-offset="100">
+          <a href="https://bosssend.com/" target="_blank">
+            <img src="./assets/images/bosssend_logo.png" alt="" width="180px">
+          </a>
+        </div>
+
+        <div class="col-md-3 wow animate__fadeInUp" data-wow-duration="2s" data-wow-delay="0.5s" data-wow-offset="100">
+          <div class="sci">
+            <h4> Connect With Us </h4>
+            <ul class="icons">
+              <li>
+                <a href="https://www.facebook.com/Mexonoil/" target="_blank"><i class="fa fa-facebook"></i> </a>
+              </li>
+
+              <li>
+                <a href=""><i class="fa fa-instagram"></i> </a>
+              </li>
+              <li>
+                <a href=""> <i class="fa fa-linkedin"></i> </a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+
+        <div class="col-md-3 wow animate__fadeInUp" data-wow-duration="2s" data-wow-delay="0.7s" data-wow-offset="100">
+          <div class="sec-contact">
+            <h4 style="color: aliceblue;"> Contact Us </h4>
+            <ul class="info">
+              <li>
+                <span><i class="fa fa-envelope" style="color: aliceblue;"></i></span>
+                <p style="color: aliceblue;">Email</p>
+              </li>
+              <li>
+                <span><i class="fa fa-phone" style="font-size:22px;color: aliceblue;"></i></span>
+                <p style="color: rgb(233, 233, 240);">+371 2 8810000</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </footer>
---
+++ resources/views/frontend/fixed/header.blade.php
@@ -0,0 +1,68 @@
+<section class="header">
+    <nav style="background-color:rgb(7, 0, 56); height: 100px;">
+      <a href="index.html">
+        <img src="./assets/images/mexon_logo.png" alt="" style="background: none;">
+      </a>
+      <div class="nav-links" id="navLinks">
+        <i class="fa fa-times" onclick="hideMenu()"></i>
+        <ul>
+          <li> <a href="#service"> Services</a></li>
+
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="product.html" id="navbarDropdown" role="button"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Products
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <!-- <li><a class="dropdown-item" href="#">Automotive</a></li>
+              <li><a class="dropdown-item" href="#">Industrial</a></li> -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="auto.html" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Automotive
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="motorbike.html">Motorbike</a></li>
+                  <li><a class="dropdown-item" href="gasoline.html">Gasoline</a></li>
+                  <li><a class="dropdown-item" href="diesel.html">Diesel</a></li>
+
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="industrial.html" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Industrial
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="industrial-diesel.html">Diesel </a></li>
+                  <li><a class="dropdown-item" href="grease.html">Grease</a></li>
+
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  Others
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Transmission</a></li>
+                  <li><a class="dropdown-item" href="#">Brake</a></li>
+                  <li><a class="dropdown-item" href="#">Coolant</a></li>
+                  <!-- <li><a class="dropdown-item" href="#">Grease</a></li> -->
+                </ul>
+              </li>
+
+            </ul>
+          </li>
+
+          <li> <a href="#business_promotion"> Business Promotion</a></li>
+          <li> <a href="contact.html"> Contacts</a></li>
+          <a href="https://bosssend.com/" target="_blank" class="buy-btn"> Buy Online</a>
+        </ul>
+
+      </div>
+      <i class="fa fa-bars" onclick="showMenu()"></i>
+    </nav>
+    <!-- header-area end -->
+  </section>
